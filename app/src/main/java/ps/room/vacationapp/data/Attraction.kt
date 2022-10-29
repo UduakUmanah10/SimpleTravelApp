bce0ca79cee2f323187b7dd59151a03fad6b9eb1
uduak <uduakumanah10@gmail.com>
2022-10-29 11:14:40 +0100

    added view holder and created the response data classes for the json object

---
+++ app/src/main/java/ps/room/vacationapp/data/Attraction.kt
@@ -0,0 +1,16 @@
+package ps.room.vacationapp.data
+
+data class Attraction(
+    val description: String = "",
+    val facts: List<String> = listOf(),
+    val id: String = "",
+    val image_urls: String = "",
+    val location: Location = Location(),
+    val months_to_visit: String = "",
+    val title: String = ""
+) {
+    data class Location(
+        val latitude: String = "",
+        val longitude: String = ""
+    )
+}
